47e8e3369f0b873a2193aba7b59763e590b89907
Verisimilitudesque <jon.schear@hotmail.com>
2026-08-27 12:29:42 -0600

    Bump plugin version to 6.3.0

--- venue-event-calendar.php
+++ venue-event-calendar.php
@@ -27,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // No direct access.
 }
 
-define( 'VEC_VERSION', '1.0.0' );
+define( 'VEC_VERSION', '6.3.0' );
 define( 'VEC_PLUGIN_FILE', __FILE__ );
 define( 'VEC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'VEC_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
